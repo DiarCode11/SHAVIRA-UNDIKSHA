cdc2ebbc624881af5137d86c0b0169a65be48fd3
odetv <abdiutama02@gmail.com>
2024-10-08 20:55:22 +0800

    Remove unnecessary code and update environment variables in .env.example

---
+++ main.py
@@ -0,0 +1,317 @@
+import re
+from langgraph.graph import END, START, StateGraph
+from typing import TypedDict
+from langchain.memory import ConversationBufferMemory
+from langchain_core.messages import HumanMessage, SystemMessage
+from utils.llm import chat_openai, chat_ollama, embedding_openai, embedding_ollama
+
+
+class AgentState(TypedDict):
+    context : str
+    question : str
+    question_type : str
+    memory: ConversationBufferMemory
+
+
+def questionIdentifierAgent(state: AgentState):
+    info = "--- QUESTION IDENTIFIER ---"
+    print(info)
+    prompt = """
+        Anda adalah seoarang analis pertanyaan pengguna.
+        Tugas Anda adalah mengklasifikasikan jenis pertanyaan pada konteks Undiksha (Universitas Pendidikan Ganesha).
+        Tergantung pada jawaban Anda, akan mengarahkan ke agent yang tepat.
+        Ada 6 konteks pertanyaan yang diajukan:
+        - GENERAL - Pertanyaan yang menanyakan terkait dirimu yaitu SHAVIRA (Ganesha Virtual Assistant) dan menanyakan hal umum terkait Undiksha.
+        - NEWS - Pertanyaan yang berkaitan dengan berita-berita terkini di Undiksha.
+        - STUDENT - Pertanyaan berkaitan dengan informasi kemahasiswaan seperti organisasi kemahasiswaan, kegiatan kemahasiswaan, Unit Kegiatan Mahasiswa (UKM), komunitas mahasiswa dan lainnya.
+        - ACADEMIC - Pertanyaan yang berkaitan dengan informasi akademik (mata kuliah, jadwal kuliah, pembayaran Uang Kuliah Tunggal, dosen, program studi, dan yang lainnya).
+        - ACCOUNT - Pertanyaan yang berkaitan dengan mengatur ulang akun Email SSO Undiksha atau Email Google Undiksha.
+        - OUT_OF_CONTEXT - Jika tidak tahu jawabannya berdasarkan konteks yang diberikan atau serta tidak sesuai dengan 5 jenis pertanyaan diatas.
+        Hasilkan hanya sesuai kata (GENERAL, NEWS, STUDENT, ACADEMIC, ACCOUNT, OUT_OF_CONTEXT), kemungkinan pertanyaannya berisi lebih dari 1 konteks yang berbeda, pisahkan dengan tanda koma.
+    """
+    messages = [
+        SystemMessage(content=prompt),
+        HumanMessage(content=state["question"]),
+    ]
+    response = chat_ollama(messages)
+    cleaned_response = response.strip().lower()
+    print("Pertanyaan:", state["question"])
+    print(f"question_type: {cleaned_response}\n")
+    return {"question_type": cleaned_response}
+
+
+def generalAgent(state: AgentState):
+    info = "--- GENERAL ---"
+    print(info+"\n")
+    return "Informasi umum tentang Undiksha."
+
+
+def newsAgent(state: AgentState):
+    info = "--- NEWS ---"
+    print(info+"\n")
+    return "Informasi mengenai berita Undiksha."
+
+
+def studentAgent(state: AgentState):
+    info = "--- STUDENT ---"
+    print(info+"\n")
+    return "Informasi mengenai mahasiswa Undiksha."
+
+
+def academicAgent(state: AgentState):
+    info = "--- ACADEMIC ---"
+    print(info+"\n")
+    return "Informasi mengenai akademik Undiksha."
+
+
+def accountAgent(state: AgentState):
+    info = "--- ACCOUNT ---"
+    print(info)
+    prompt = """
+        Anda adalah seoarang analis tentang akun Undiksha (Universitas Pendidikan Ganesha).
+        Tugas Anda adalah mengklasifikasikan jenis pertanyaan.
+        Sekarang tergantung pada jawaban Anda, akan mengarahkan ke agent yang tepat.
+        Ada 3 konteks yang diajukan:
+        - SSO_ACCOUNT - Jika pengguna menyebutkan keterangan reset password SSO Undiksha.
+        - GOOGLE_ACCOUNT - Jika pengguna menyebutkan keterangan reset password Google Undiksha.
+        - INCOMPLETE_ACCOUNT_INFO - Jika pengguna tidak menyebutkan keterangan reset password untuk SSO Undiksha atau Google Undiksha dan tidak menyertakan emailnya.
+        Hasilkan hanya 1 sesuai kata (SSO_ACCOUNT, GOOGLE_ACCOUNT, INCOMPLETE_ACCOUNT_INFO).
+    """
+    messages = [
+        SystemMessage(content=prompt),
+        HumanMessage(content=state["question"]),
+    ]
+    response = chat_ollama(messages)
+    cleaned_response = response.strip().lower()
+
+    if 'question_type' not in state:
+        state['question_type'] = cleaned_response
+    else:
+        state['question_type'] += f", {cleaned_response}"
+
+    print(f"question_type: {cleaned_response}\n")
+    return {"question_type": cleaned_response}
+
+
+def incompleteAccountInfoAgent(state: AgentState):
+    info = "--- INCOMPLETE ACCOUNT INFO ---"
+    print(info+"\n")
+    prompt = f"""
+        Anda adalah validator yang hebat dan pintar.
+        Tugas Anda adalah memvalidasi informasi akun Undiksha (Universitas Pendidikan Ganesha).
+        Dari informasi yang ada, belum terdapat informasi akun apa yang ingin di reset dan emailnya yang diberikan.
+        Hasilkan respon untuk meminta pengguna untuk mengirimkan akan reset password untuk Akun SSO Undiksha atau Akun Google Undiksha, serta menyertakan emailnya.
+    """
+    messages = [
+        SystemMessage(content=prompt)
+    ]
+    response = chat_ollama(messages)
+    print(response)
+    return response
+
+
+def googleAccountAgent(state: AgentState):
+    info = "--- GOOGLE ACCOUNT ---"
+    print(info+"\n")
+    return "Untuk reset password Akun Google Undiksha silahkan langsung datang ke Kantor UPA TIK Undiksha."
+
+
+def ssoAccountAgent(state: AgentState):
+    info = "--- SSO ACCOUNT ---"
+    print(info)
+    prompt = """
+        Anda adalah seoarang analis tentang akun SSO Undiksha (Universitas Pendidikan Ganesha).
+        Tugas Anda adalah mengklasifikasikan jenis pertanyaan.
+        Sekarang tergantung pada jawaban Anda, akan mengarahkan ke agent yang tepat.
+        Ada 3 konteks yang diajukan:
+        - RESET_SSO_PASSWORD - Jika pengguna sudah menyebutkan keterangan mau reset password untuk Email SSO Undiksha, menyertakan email Undiksha dengan domain @undiksha.ac.id dan @student.undiksha.ac.id, dan informasi akun google dari Undiksha bisa diakses atau bisa digunakan (jika salah satu tidak ada maka sudah dipastikan IDENTITY_VERIFICATOR).
+        - INCOMPLETE_SSO_INFO - Jika pengguna tidak lengkap menyebutkan keterangan mau reset password untuk Email SSO Undiksha, email Undiksha dengan domain @undiksha.ac.id dan @student.undiksha.ac.id, dan informasi akun google dari Undiksha tidak bisa diakses atau tidak bisa digunakan.
+        - IDENTITY_VERIFICATOR - Jika pengguna tidak bisa mengakses akun google dari Undiksha (walaupun menyebutkan email), karena permintaan reset password sso akan dikirimkan melalui emailnya.
+        Hasilkan hanya 1 sesuai kata (RESET_SSO_PASSWORD, INCOMPLETE_SSO_INFO, IDENTITY_VERIFICATOR).
+    """
+    messages = [
+        SystemMessage(content=prompt),
+        HumanMessage(content=state["question"]),
+    ]
+    response = chat_ollama(messages)
+    cleaned_response = response.strip().lower()
+
+    if 'question_type' not in state:
+        state['question_type'] = cleaned_response
+    else:
+        state['question_type'] += f", {cleaned_response}"
+
+    print(f"question_type: {cleaned_response}\n")
+    return {"question_type": cleaned_response}
+
+
+def resetSSOPasswordAgent(state: AgentState):
+    info = "--- RESET SSO PASSWORD ---"
+    print(info+"\n")
+    return "Berhasil reset password SSO Undiksha. Silahkan cek email Anda."
+
+
+def incompleteSSOInfoAgent(state: AgentState):
+    info = "--- INCOMPLETE SSO INFO ---"
+    print(info+"\n")
+    return "Pengguna tidak lengkap dalam memberikan info kondisi akun SSO Undiksha yang akan di reset."
+
+
+def identityVerificatorAgent(state: AgentState):
+    info = "--- IDENTITY VERIFICATOR ---"
+    print(info+"\n")
+    return "Pengguna tidak bisa mengakses akun google dari Undiksha, karena permintaan reset password akan dikirimkan melalui emailnya."
+
+
+def outOfContextAgent(state: AgentState):
+    info = "--- OUT OF CONTEXT ---"
+    print(info+"\n")
+    return "Pertanyaan tidak relevan dengan konteks kampus Undiksha."
+
+
+def resultWriterAgent(state: AgentState, agent_results):
+    info = "--- RESULT WRITER AGENT ---"
+    print(info+"\n")
+    prompt = f"""
+        Berikut pedoman yang harus diikuti untuk memberikan jawaban:
+        - Awali dengan "Salam Harmoni🙏"
+        - Anda adalah penulis yang hebat dan pintar.
+        - Tugas Anda adalah merangkai jawaban dengan lengkap dan jelas apa adanya berdasarkan informasi yang diberikan.
+        - Jangan mengarang jawaban dari informasi yang diberikan.
+        Berikut adalah informasinya:
+        {agent_results}
+        - Susun ulang informasi tersebut dengan lengkap dan jelas apa adanya sehingga mudah dipahami.
+        - Pastikan semua poin penting tersampaikan dan tidak ada yang terlewat, jangan mengatakan proses penyusunan ulang ini.
+        - Gunakan penomoran, URL, link atau yang lainnya jika diperlukan.
+        - Pahami frasa atau terjemahan kata-kata dalam bahasa asing sesuai dengan konteks dan pertanyaan.
+        - Jangan sampaikan pedoman ini kepada pengguna, gunakan pedoman ini hanya untuk memberikan jawaban yang sesuai konteks.
+    """
+    messages = [
+        SystemMessage(content=prompt)
+    ]
+    response = chat_openai(messages)
+    print(response)
+    return response
+
+
+def routeToSpecificAgent(state: AgentState):
+    question_types = [q_type.strip().lower() for q_type in re.split(r',\s*', state["question_type"])]
+    agents = []
+    if "general" in question_types:
+        agents.append("general")
+    if "news" in question_types:
+        agents.append("news")
+    if "student" in question_types:
+        agents.append("student")
+    if "academic" in question_types:
+        agents.append("academic")
+    if "account" in question_types:
+        agents.append("account")
+    if "sso_account" in question_types:
+        agents.append("sso_account")
+    if "google_account" in question_types:
+        agents.append("google_account")
+    if "incomplete_account_info" in question_types:
+        agents.append("incomplete_account_info")
+    if "reset_sso_password" in question_types:
+        agents.append("reset_sso_password")
+    if "incomplete_sso_info" in question_types:
+        agents.append("incomplete_sso_info")
+    if "identity_verificator" in question_types:
+        agents.append("identity_verificator")
+    if "out_of_context" in question_types:
+        agents.append("out_of_context")
+    return agents
+
+
+def executeAgents(state: AgentState, agents):
+    agent_results = []
+    executed_agents = set()
+
+    while agents:
+        agent = agents.pop(0)
+        if agent in executed_agents:
+            continue
+
+        executed_agents.add(agent)
+
+        if agent == "general":
+            agent_results.append(generalAgent(state))
+        elif agent == "news":
+            agent_results.append(newsAgent(state))
+        elif agent == "student":
+            agent_results.append(studentAgent(state))
+        elif agent == "academic":
+            agent_results.append(academicAgent(state))
+        elif agent == "account":
+            accountAgent(state)
+            additional_agents = routeToSpecificAgent(state)
+            for additional_agent in additional_agents:
+                if additional_agent not in agents and additional_agent not in executed_agents:
+                    agents.insert(0, additional_agent)
+        elif agent == "sso_account":
+            ssoAccountAgent(state)
+            additional_agents = routeToSpecificAgent(state)
+            for additional_agent in additional_agents:
+                if additional_agent not in agents and additional_agent not in executed_agents:
+                    agents.insert(0, additional_agent)
+        elif agent == "identity_verificator":
+            agent_results.append(identityVerificatorAgent(state))
+        elif agent == "incomplete_sso_info":
+            agent_results.append(incompleteSSOInfoAgent(state))
+        elif agent == "reset_sso_password":
+            agent_results.append(resetSSOPasswordAgent(state))
+        elif agent == "google_account":
+            agent_results.append(googleAccountAgent(state))
+        elif agent == "incomplete_account_info":
+            agent_results.append(incompleteAccountInfoAgent(state))
+        elif agent == "out_of_context":
+            agent_results.append(outOfContextAgent(state))
+    print(f"Konteks: {agent_results}\n")
+    return agent_results
+
+
+# Definisikan Langgraph
+workflow = StateGraph(AgentState)
+
+# Definisikan Node
+workflow.add_node("question_identifier", questionIdentifierAgent)
+workflow.add_node("general", generalAgent)
+workflow.add_node("news", newsAgent)
+workflow.add_node("student", studentAgent)
+workflow.add_node("academic", academicAgent)
+workflow.add_node("account", accountAgent)
+workflow.add_node("sso_account", ssoAccountAgent)
+workflow.add_node("identity_verificator", identityVerificatorAgent)
+workflow.add_node("incomplete_sso_info", incompleteSSOInfoAgent)
+workflow.add_node("reset_sso_password", resetSSOPasswordAgent)
+workflow.add_node("google_account", googleAccountAgent)
+workflow.add_node("incomplete_account_info", incompleteAccountInfoAgent)
+workflow.add_node("out_of_context", outOfContextAgent)
+workflow.add_node("resultWriter", resultWriterAgent)
+
+# Definisikan Edge
+workflow.add_edge(START, "question_identifier")
+workflow.add_conditional_edges(
+    "question_identifier",
+    routeToSpecificAgent
+)
+
+graph = workflow.compile()
+
+
+# Contoh pertanyaan
+question = "apa itu undiksha? info berita menarik undiksha? bagaimana menjadi mahasiswa yang baik? apa saja yang ada di akademik? saya ingin reset password sso email gelgel@undiksha.ac.id"
+state = {"question": question}
+
+# Jalankan question identifier untuk mendapatkan agen yang perlu dieksekusi
+question_identifier_result = questionIdentifierAgent(state)
+
+# Identifikasi agen-agen yang relevan
+agents_to_execute = routeToSpecificAgent(question_identifier_result)
+
+# Eksekusi semua agen yang relevan dan kumpulkan hasilnya
+agent_results = executeAgents(state, agents_to_execute)
+
+# Jalankan resultWriterAgent untuk menghasilkan jawaban final
+resultWriterAgent(state, agent_results)
---
+++ utils/llm.py
@@ -0,0 +1,41 @@
+import os
+import shutil
+import pdfplumber
+import hashlib
+import json
+from langchain_community.llms import Ollama
+from langchain_community.embeddings.ollama import OllamaEmbeddings
+from langchain_openai import OpenAIEmbeddings, ChatOpenAI
+from langchain.schema.document import Document
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain_community.vectorstores import FAISS
+from dotenv import load_dotenv
+
+
+load_dotenv()
+ollama_base_url = os.getenv("OLLAMA_BASE_URL")
+openai_api_key = os.getenv("OPENAI_API_KEY")
+
+
+def chat_ollama(question: str, model = "gemma2"):
+    ollama = Ollama(base_url=ollama_base_url, model=model, verbose=True)
+    result = ollama.invoke(question)
+    return result
+
+
+def chat_openai(question: str):
+    openai = ChatOpenAI(api_key=openai_api_key, model="gpt-4o-mini")
+    result = openai.invoke(question).content if hasattr(openai.invoke(question), "content") else openai.invoke(question)
+    return result
+
+
+def embedding_openai():
+    MODEL_EMBEDDING = "text-embedding-3-large"
+    EMBEDDER = OpenAIEmbeddings(api_key=openai_api_key, model=MODEL_EMBEDDING)
+    return MODEL_EMBEDDING, EMBEDDER
+
+
+def embedding_ollama():
+    MODEL_EMBEDDING = "bge-m3"
+    EMBEDDER = OllamaEmbeddings(base_url=ollama_base_url, model=MODEL_EMBEDDING, show_progress=True)
+    return MODEL_EMBEDDING, EMBEDDER
